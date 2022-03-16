579ab3290cb2a3c63d15e41d4217babe243b8ab0
74929911@certus.edu.pe <74929911@certus.edu.pe>
2022-03-16 16:35:04 -0500

    commit3

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ box.addEventListener("click", (e) => {
         box.setAttribute("src", "assets/img/cofre_minecraft_abierto.png");
         box.innerHTML = "<audio src='assets/sonido_de_cofre_abierto_minecraft.mp3' autoplay></audio>"
         box.classList.add("luz");
-        alert("Te amo mucho chiquis")
+        alert("Te amo mucho mi princesita ❤");
         cambio = 1;
     } else {
         box.setAttribute("src", "assets/img/cofre_minecraft_cerrado.png");
